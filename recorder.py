818342ad383bde638fe0a5b88c9a221489b483e2
caseyanderson <cta@caseyanderson.com>
2018-02-20 10:47:44 -0800

    taking a stab at this

---
+++ recorder.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+'''
+wrapper for controlling arecord instances
+'''
+
+import subprocess
+import os
+import signal
+
+
+class Recorder():
+
+    def record(self, path):
+        path = self.path
+        self.process = subprocess.Popen(['arecord', '-f', 'cd', '-D', 'plughw:1', path], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+    # def record_dur(self, path, duration):
+    #     path = self.path
+    #     duration = self.duration
+    #     self.process =
+
+    def status(self):
+        if self.process.poll() is not None:
+            return 'done'
+        else:
+            return 'playing'
+
+    # def stop(self):
+    #     self.process.stdin.write(b'q')
+    #     self.process.stdin.flush()
+
+
+
+    # def kill(self):
+    #     os.killpg(os.getpgid(self.process.pid), signal.SIGTERM)
+    #
+    # def toggle(self):
+    #     self.process.stdin.write(b'p')
+    #     self.process.stdin.flush()
